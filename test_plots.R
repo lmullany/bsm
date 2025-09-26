c7856d1be662cd339c13996a9eeda5a8a3df0796
Mark Panaggio <mark.panaggio@jhuapl.edu>
2025-09-26 13:07:22 -0400

    Add one more.

--- test_plots.R
+++ test_plots.R
@@ -1,3 +1,6 @@
+# © 2025 The Johns Hopkins University Applied Physics Laboratory LLC
+# Development of this software was sponsored by the U.S. Government under
+# contract no. 75D30124C19958
 
 
 load("test_data_md.RData")
