2957e00ed6f6b475dd2bd4a1827e89f4784f7ab2
Mark Panaggio <mark.panaggio@jhuapl.edu>
2025-11-04 09:33:17 -0500

    Add seasonal component.

--- src/modules/inla_model.R
+++ src/modules/inla_model.R
@@ -18,9 +18,9 @@ inla_model_ui <- function(id) {
     ns("dist_family"),
     label="Distributional Family",
     choices = c(
-      "Binomial" = "binomial"#,
-      #"Negative Binomial"="nbinomial",
-      #"Poisson" = "poisson"
+      "Binomial" = "binomial",
+      "Negative Binomial"="nbinomial",
+      "Poisson" = "poisson"
     ),
     selected = "binomial"
   )
@@ -121,7 +121,7 @@ inla_model_ui <- function(id) {
     div(
       class="well",
       layout_columns(
-        checkboxInput(ns("temporal_component_chkbx"), "Seaonal/Temporal Component",value = FALSE),
+        checkboxInput(ns("temporal_component_chkbx"), "Seasonal/Temporal Component",value = FALSE),
         input_switch(ns("customize_temporal_component"),label ="Advanced Customization",value = FALSE),
         col_widths = c(6,6)
       ), 
@@ -535,13 +535,14 @@ pre_process_data <- function(data, nforecasts ) {
   )
   
 
-data_cls <- epistemic::add_missing_and_future_dates(
-  num_future_steps = nforecasts,
-  dc = data_cls,
-  forward_fill = TRUE,
-  den = 1
-)
+  data_cls <- epistemic::add_missing_and_future_dates(
+    num_future_steps = nforecasts,
+    dc = data_cls,
+    forward_fill = TRUE,
+    den = 1
+  )
 
+  data_cls <- epistemic::add_mmwr_week(data_cls)
   return(data_cls)
 }
 
